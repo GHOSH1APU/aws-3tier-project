d711a5a08b534e61ca14d6ff7415f88a3f721a9a
Sourav <ghoshsourav21@gmail.com>
2026-09-20 22:51:50 +0530

    change desired capcity ASG

--- asg.tf
+++ asg.tf
@@ -92,7 +92,7 @@ resource "aws_autoscaling_group" "app_asg" {
 
   min_size         = 1
   max_size         = 3
-  desired_capacity = 1
+  desired_capacity = 2
 
   launch_template {
     id      = aws_launch_template.app_lt.id
