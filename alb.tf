844f9f659f7870a1ccdba005fe2034ea4a3ea756
Sourav <ghoshsourav21@gmail.com>
2026-09-20 21:22:32 +0530

    Add ALB DNS Output

--- alb.tf
+++ alb.tf
@@ -35,3 +35,8 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.app_tg.arn
   }
 }
+
+output "alb_dns_name" {
+  value       = aws_lb.app_alb.dns_name
+  description = "The DNS name of the ALB to access the web application"
+}
