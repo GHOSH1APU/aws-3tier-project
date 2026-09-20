621a5b63b360f214360c955cd37226841d03f649
Sourav <ghoshsourav21@gmail.com>
2026-09-20 20:52:05 +0530

    Export subnets and Create ALB with TG

---
+++ alb.tf
@@ -0,0 +1,37 @@
+# 1. The Application Load Balancer
+resource "aws_lb" "app_alb" {
+  name               = "3-tier-app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = module.networking.public_subnets
+}
+
+# 2. Target Group (Routes traffic to the App Servers)
+resource "aws_lb_target_group" "app_tg" {
+  name     = "3-tier-app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.networking.vpc_id
+
+  health_check {
+    path                = "/"
+    healthy_threshold   = 2
+    unhealthy_threshold = 5
+    timeout             = 5
+    interval            = 30
+    matcher             = "200"
+  }
+}
+
+# 3. ALB Listener (Listens on port 80 and forwards to the Target Group)
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -3,3 +3,15 @@ output "vpc_id" {
   value       = aws_vpc.main.id
   description = "The ID of the VPC"
 }
+
+output "public_subnets" {
+  value = [aws_subnet.public_1a.id, aws_subnet.public_1b.id]
+}
+
+output "private_app_subnets" {
+  value = [aws_subnet.private_app_1a.id, aws_subnet.private_app_1b.id]
+}
+
+output "private_db_subnets" {
+  value = [aws_subnet.private_db_1a.id, aws_subnet.private_db_1b.id]
+}
